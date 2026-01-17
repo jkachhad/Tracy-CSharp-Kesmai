9c42c5e6e4b96d13908e2746f2b5f2f8efd465b2
jkachhad <jalpeshk@gmail.com>
2026-01-17 15:28:49 -0800

    Logging

--- src/c/production/Tracy/_Tracy.h
+++ src/c/production/Tracy/_Tracy.h
@@ -1,22 +1,2 @@
-// Force re-evaluation of TracyApi.h even if something included it earlier.
-#ifdef __TRACYAPI_H__
-#undef __TRACYAPI_H__
-#endif
-
-#ifndef TRACY_ENABLE
-#define TRACY_ENABLE 1
-#endif
-
-// Make TRACY_API non-empty on Windows.
-#ifndef TRACY_IMPORTS
-#define TRACY_IMPORTS 1
-#endif
-
-#include "public/common/TracyApi.h"
-
-// Now include TracyC.h (and also force it to re-evaluate if it was included earlier).
-#ifdef __TRACYC_HPP__
-#undef __TRACYC_HPP__
-#endif
-
+#define TRACY_ENABLE
 #include "public/tracy/TracyC.h"
