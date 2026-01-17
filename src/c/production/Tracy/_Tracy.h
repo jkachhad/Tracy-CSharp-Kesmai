7a5121a668ec3fa37b6d54da152a446c105b05b5
jkachhad <jalpeshk@gmail.com>
2026-01-17 12:59:40 -0800

    Update _Tracy.h to support c2ffi

--- src/c/production/Tracy/_Tracy.h
+++ src/c/production/Tracy/_Tracy.h
@@ -1,2 +1,12 @@
 #define TRACY_ENABLE
+
+#if defined(__clang__)
+  #undef TRACY_API
+  #if defined(_WIN32)
+    #define TRACY_API extern __declspec(dllimport) __attribute__((visibility("default")))
+  #else
+    #define TRACY_API extern __attribute__((visibility("default")))
+  #endif
+#endif
+
 #include "public/tracy/TracyC.h"
